40458378b1c68c74843a23690e9cd332768b9af8
NME_ <skaxo0823@gmail.com>
2020-05-14 08:51:02 +0900

    modified resource

--- sw300-navi/Resource.h
+++ sw300-navi/Resource.h
@@ -13,6 +13,7 @@
 #define IDC_SCROLLBAR2                  1002
 #define IDC_COMBO1                      1003
 #define IDC_RADIO1                      1004
+#define IDC_RADIO2                      1005
 
 // Next default values for new objects
 // 
@@ -20,7 +21,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
