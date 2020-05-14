564ee3337aac2e074324a695a35bdb24841e9675
naviworks7 <naviworks7@gmail.com>
2020-05-14 17:25:15 +0900

    소켓서버클라이언트

---
+++ Socket_new/SocketClient/SocketClient/CAlias.cpp
@@ -0,0 +1,71 @@
+﻿// CAlias.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "SocketClient.h"
+#include "CAlias.h"
+#include "afxdialogex.h"
+#include "SocketClientDlg.h"
+
+
+// CAlias 대화 상자
+
+IMPLEMENT_DYNAMIC(CAlias, CDialogEx)
+
+CAlias::CAlias(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+
+}
+
+CAlias::~CAlias()
+{
+}
+
+void CAlias::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CAlias, CDialogEx)
+	ON_BN_CLICKED(IDC_BUTTON1, &CAlias::OnBnClickedButton1)
+END_MESSAGE_MAP()
+
+
+// CAlias 메시지 처리기
+
+// 별명 설정 클릭
+void CAlias::OnBnClickedButton1()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	CString str;
+	CString alias = L"alias:";
+
+
+	GetDlgItemText(IDC_EDIT1, str);	// EDIT 컨트롤에 있는 닉네임 값 가져오기
+
+	((CSocketClientDlg*)GetParent())->name = str;
+
+	str = alias + str;
+
+	// alias : 이름    -> 서버로 전송
+	// 이 형식의 메시지는 이름 설정용으로 이름 저장됨
+	((CSocketClientDlg*)GetParent())->m_Socket.Send((LPVOID)(LPCTSTR)str, str.GetLength() * 2);
+
+	::SendMessage(this->m_hWnd, WM_CLOSE, NULL, NULL);
+}
+
+BOOL CAlias::PreTranslateMessage(MSG* pMsg)
+{
+	if (pMsg->message == WM_KEYDOWN)
+	{
+		if (pMsg->wParam == VK_RETURN)
+		{
+			// Enter 키 지원용
+			OnBnClickedButton1();
+			return true;
+		}
+	}
+	return CDialogEx::PreTranslateMessage(pMsg);
+}
